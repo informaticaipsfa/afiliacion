4e77f655080b123ab0182bfbceda0ce35426614d
Carlos Peña <gesaodin@gmail.com>
2019-06-25 10:14:08 -0400

    Medida Pension I

--- js/es6/voucher.js
+++ js/es6/voucher.js
@@ -1,7 +1,4 @@
-         
 function ImprimirNeto(){
-    
-
     if ( $("#cmbNetoPago option:selected").val() == "X" ) {        
         return false;
     }
@@ -17,10 +14,8 @@ function ImprimirNeto(){
     var pos = $("#cmbNetoPago option:selected").val();
     var objNeto = [];
     var totalAsignacion = 0;
-
     var obj = lstNeto[pos];
     console.log(lstNeto);
-
 	for(var i=0; i< obj.length; i++){
 		var monto = obj[i].mont;
         var tipo = obj[i].tipo;
@@ -29,7 +24,6 @@ function ImprimirNeto(){
         var montostr = accounting.formatMoney(monto, "Bs. ", 2, ".", ",");
         objNeto[lbl] = numeral(parseFloat(monto,2)).format('0,0.00');
         var grado = $("#cmbgrado option:selected").text();
-        //console.log(obj[i]);
 		if(tipo == 97){      
            
             totalAsignacion += monto;
