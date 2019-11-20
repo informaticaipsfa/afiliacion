84904dbeb42cf4976b1d39f232d2fdb24d1f173c
Carlos Peña <gesaodin@gmail.com>
2019-11-19 20:38:44 -0400

    Detalles del Credito: Tabla de Amortización

--- js/es6/credito.js
+++ js/es6/credito.js
@@ -1,4 +1,30 @@
+let opcionesCredito = {
+    ordering: false,
+    paging: false,            
+    scrollY:        200,
+    deferRender:    true,
+    scroller:       true,
+    language: {
+        "lengthMenu": "Mostar _MENU_ filas por pagina",
+        "zeroRecords": "Nada que mostrar",
+        "info": "Mostrando _PAGE_ de _PAGES_",
+        "infoEmpty": "No se encontro nada",
+        "infoFiltered": "(filtered from _MAX_ total records)",
+        "search": "Buscar"
+    }
+};
 
+class Credito{
+	constructor(){
+		this.cuota = 0;
+		this.periodo = 0;
+		this.monto = 0.0;
+		this.sueldo = 0.0;
+	}
+	Crear(req){
+
+	}
+}
 
 
 function IniciarCredito(estatus){
@@ -19,4 +45,65 @@ function IniciarCredito(estatus){
 
 function CargarCredito( str ){
 	StepperCredito.next();
+}
+
+function CalcularCredito(){
+	var monto = parseFloat( $("#txtMontoCr").val() ) * 1; //  solicitamos la cantidad prestada, el plazo y el tipo de interes
+	var interes = parseFloat( $("#txtInteresCr").val() ) / (100 * 12);//  multiplicamos por 100, para disolver el %, y por 12, para tener valor mensual
+	var periodo = parseFloat( $("#cmbCuotasCr").val() ) * 1 * 12;// multiplicamos por 12 para devolver valor mensual
+	
+	var potencia = 1 + interes;
+	var xxx = Math.pow(potencia, -periodo);//  funcion matematica donde la base es la potencia y el exponente el tiempo
+	
+	var xxx1 = monto * interes;
+	var equivalencia = xxx1 / (1 - xxx);
+
+	equivalencia = parseFloat(equivalencia); //  limitamos el número de decimales a cero
+	return equivalencia;
+}
+
+function TablaAmortizacion(){
+	$("#_TblAmortizacion").html(HTMLTblAmortizacion());        
+	var t = $('#tblCredito').DataTable(opcionesCredito);
+	t.clear().draw();
+	var monto = parseFloat( $("#txtMontoCr").val() ) * 1;
+	var cuota = CalcularCredito();
+	var periodo = parseInt( $("#cmbCuotasCr").val() ) * 1 * 12;
+	var interes = parseFloat( $("#txtInteresCr").val() ) / (100 * 12);
+	for (var i = 0; i < periodo; i++) {
+		
+		var ainteres = monto * interes;
+		var capital = cuota - ainteres;
+		var saldo = monto - ainteres - capital;
+		t.row.add([
+			i, //#
+			parseFloat( monto ).toFixed(2), //Balance
+			parseFloat( cuota ).toFixed(2), //Cuota
+			parseFloat( ainteres ).toFixed(2), //Interes
+			parseFloat( capital ).toFixed(2), //Capital
+			parseFloat( saldo ).toFixed(2), //Saldo
+			'' 
+		]).draw(false);
+
+		monto =  monto - cuota;
+	}
+	StepperCredito.next();
+}
+
+
+function HTMLTblAmortizacion(){
+	return `
+	<table id="tblCredito" class="ui celled table table-bordered table-striped dataTable" >
+		<thead>
+			<tr role="row">
+				<th>#</th>
+				<th>BALANCE</th>
+				<th>CUOTA</th>
+				<th>INTERES</th>                                            
+				<th>CAPITAL</th>                   
+				<th>SALDO</th>
+				<th>FECHA</th>
+			</tr>
+		</thead>
+	</table>`;
 }
