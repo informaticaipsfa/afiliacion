06e591b53ba58a7f40bf121bbf16db4c26715b3b
Carlos Peña <gesaodin@gmail.com>
2019-12-18 10:59:42 -0400

    Calculadora

--- js/es6/calculadora.js
+++ js/es6/calculadora.js
@@ -139,7 +139,7 @@ class WCalc{
     Crear(req){
         
         wCalculos = req;
-        $('#cmbAguinaldos').html("");
+        $('#cmbAguinaldos').html(`<option value='0'>NO APLICA CALCULOS</option>`);
         
 
 
